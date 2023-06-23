1253378f929e6e027d976d05e6b03159f7028252
Isaac-ngahu <isaac.ngahu@student.moringaschool.com>
2023-06-23 16:50:25 +0300

    first commit

---
+++ calculator.rb
@@ -0,0 +1,33 @@
+def simple_calculator
+    print "Enter first number: "
+    first_input = $stdin.gets.chomp
+    first_number = first_input.to_f
+    print "Enter second number: "
+    second_input = $stdin.gets.chomp
+    second_number = second_input.to_f
+    print "Choose the operation you would like to perform +,-,/,*: "
+    operand = $stdin.gets.chomp
+    if second_number.class != Float or first_number.class != Float
+         return "Please enter correct format of numbers"
+    elsif operand == "+"
+        sum = first_number + second_number
+        return sum
+    elsif operand == "-"
+        difference = first_number - second_number
+        return difference
+    elsif operand == "/"
+        quotient = first_number/second_number
+        return quotient
+    elsif operand == "*"
+        product = first_number*second_number
+        return product
+    else
+        return "enter correct operand"
+    end
+
+end
+ 
+puts simple_calculator
+
+
+
