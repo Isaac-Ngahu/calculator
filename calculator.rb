e556851ef2f348505b569dc95aa0a42c04a8a5a8
Isaac-ngahu <isaac.ngahu@student.moringaschool.com>
2023-06-26 10:40:22 +0300

    adding result messages

--- calculator.rb
+++ calculator.rb
@@ -1,24 +1,24 @@
 def simple_calculator
     print "Enter first number: "
-    first_input = $stdin.gets.chomp
+    first_input = gets.chomp()
     first_number = first_input.to_f
     print "Enter second number: "
-    second_input = $stdin.gets.chomp
+    second_input = gets.chomp()
     second_number = second_input.to_f
     print "Choose the operation you would like to perform +,-,/,*: "
     operand = $stdin.gets.chomp
     if operand == "+"
         sum = first_number + second_number
-        return sum
+        return "sum is #{sum}"
     elsif operand == "-"
         difference = first_number - second_number
-        return difference
+        return "difference is #{difference}"
     elsif operand == "/"
         quotient = first_number/second_number
-        return quotient
+        return "quotient is #{quotient}"
     elsif operand == "*"
         product = first_number*second_number
-        return product
+        return "product is #{product}"
     else
         return "enter correct operation"
     end
